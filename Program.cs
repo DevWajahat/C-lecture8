b8782bc8426b7e0865c61bbcad15b476a2dae28d
Wajahat Abbas <155806602+DevWajahat@users.noreply.github.com>
2024-10-21 20:42:24 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Grade obj = new Grade();
+        obj.printsdetails();
+
+
+    }
+}
+
+class Animal
+{
+
+    public virtual void Eat()
+    {
+        Console.WriteLine("Every Animal eat something");
+    }
+
+}
+
+class Cat : Animal
+{
+    public override void Eat()
+    {
+        Console.WriteLine("Cats Eats to love fish...");
+    }
+
+}
+
+
+class student
+{
+
+    string _stdName = "Rizwan";
+    string _address = "karachi";
+    public virtual void printsdetails()
+    {
+        Console.WriteLine("Student Name is " + _stdName);
+        Console.WriteLine("Student Address is " + _address);
+    }
+
+}
+class Grade : student
+{
+    string _class = "Four";
+    float _percent = 71.87f;
+
+    public override void printsdetails()
+    {
+        Console.WriteLine("Student Class is " + _class);
+        Console.WriteLine("Student percentage is " + _percent);
+    }
+}
